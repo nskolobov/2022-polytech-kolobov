8163cfa1e0b1138d42e87def7ff48acad8528bcb
Kolobov Nikita <kolobov.ns@edu.spbstu.ru>
2022-09-30 16:19:24 +0300

    feat(array-insertion): add search_position

--- 04_search_sort/array_insertion/array-insertion.cpp
+++ 04_search_sort/array_insertion/array-insertion.cpp
@@ -5,6 +5,11 @@ int search_position(int arr[], int n, int x);
 
 int main() {
   // NOTE: aN - array
+  //       rN - result
+  // `insert` example
+  int a0[10] =          {0, 1, 2, 3,      4, 5, 6, 7, 8, 9};
+  int expected_r0[11] = {0, 1, 2, 3, 100, 4, 5, 6, 7, 8, 9};
+  auto r0 = insert(a0, 10, 100, 4);
   for (auto i = 0; i < 11; ++i)
     assert(r0[i] == expected_r0[i]);
 
@@ -17,7 +22,11 @@ int main() {
 
   return 0;
 }
-arr[p] = x;
+
+int *insert(int arr[], int n, int x, int p) {
+  for (auto i = n; i > p; i--)
+    arr[i] = arr[i - 1];
+  arr[p] = x;
   return arr;
 }
 
