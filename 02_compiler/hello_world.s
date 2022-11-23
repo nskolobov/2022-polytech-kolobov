d339ab8c2cf1fc7813a242f443c025af76f80f0e
Kolobov Nikita <kolobov.ns@edu.spbstu.ru>
2022-11-23 18:05:30 +0300

    :assembler

---
+++ 02_compiler/hello_world.s
@@ -0,0 +1,25 @@
+.file	"hello_world.cpp"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB0:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	call	_Z5printv
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev6, Built by MSYS2 project) 12.2.0"
+	.def	_Z5printv;	.scl	2;	.type	32;	.endef
+
