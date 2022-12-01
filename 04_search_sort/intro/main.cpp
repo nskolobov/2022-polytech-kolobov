8b09478d732b1498b09a16b941c11ef638153865
Kolobov Nikita <kolobov.ns@edu.spbstu.ru>
2022-12-02 02:29:26 +0300

    :added

---
+++ 04_search_sort/intro/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+int linear_search(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        if (arr[i] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binary_search(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+
+        // Проверяем
+        if (arr[mid] == x)
+            return mid;
+
+        // Идём в левую часть
+        if (arr[mid] > x)
+            return binary_search(arr, l, mid - 1, x);
+
+        // Идём в правую часть
+        return binary_search(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = { -996, -960, -944, -887, -865, -860, -846, -831, -790, -785, -700, -691, -662, -662, -647,
+                  -640, -587, -532, -524, -519, -501, -497, -475, -471, -461, -457, -452, -442, -441, -416,
+                  -412, -411, -390, -389, -358, -341, -306, -257, -253, -252, -248, -208, -143, -134, -82,
+                  -79, -11, 21, 26, 48, 68, 69, 72, 93, 95, 99, 115, 120, 156, 164, 181, 200, 231, 252, 288,
+                  304, 336, 343, 364, 402, 419, 486, 546, 584, 595, 596, 606, 627, 636, 666, 666, 683, 701,
+                  740, 766, 780, 782, 794, 802, 829, 832, 837, 858, 875, 881, 882, 883, 883, 947, 959};
+    int x = 120;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result_linear_search = linear_search(arr, n, x);
+    if (result_linear_search == -1)
+    {
+        std::cout << "Element is not present in array" << std::endl;
+    }
+    else
+    {
+        std::cout << "Element is present at index " << result_linear_search << std::endl;
+    }
+    int result_binary_search = binary_search(arr, 0, n - 1, x);
+    if (result_binary_search == -1)
+    {
+        std::cout << "Element is not present in array" << std::endl;
+    }
+    else
+    {
+        std::cout << "Element is present at index " << result_binary_search << std::endl;
+    }
+    return 0;
+}
